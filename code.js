1f945053f0c261105af6d8a68e40f3b0429e0759
NatalyaDol <suhoikova@gmail.com>
2019-03-02 21:36:45 +0300

    Added the versions tasks by teacher

--- code.js
+++ code.js
@@ -4,21 +4,23 @@
 Например, для числа 245 надо получить следующий объект: {‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}. 
 Если число превышает 999, 
 необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект. */
+//1 вариант решения
 console.log('Задание №1');
-var obj = {};
-var numInObj = function(number) {
-    var answer = prompt('Введите трехзначное число');
-   if (answer < 999) {
-        obj['единицы'] = parseInt(answer[2]);
-        obj['десятки'] = parseInt(answer[1]);
-        obj['сотни'] = parseInt(answer[0]);
-        console.log(obj);
-   } else {
-    console.log('Вы ввели не трехзначное число ' + obj);
-   } 
-}; 
-numInObj();
+var getObj = function () {
+   let num = + prompt('Введите число от 0 до 999: ');
+   let obj = {};
 
+   if (num < 1 || num > 999) {
+      console.log('Введите число от 0 до 999:');
+      return obj;
+   } else {
+      obj.units = num % 10;
+      obj.tens = (num - obj.units) % 100 / 10;
+      obj.hundr = (num - obj.units - obj.tens * 10) % 1000 / 100;
+      return obj;
+   }
+} 
+console.log(getObj());
 
 console.log('Задание №2 с корзиной интернет магазина Объекты');
 /*Продолжить работу с интернет-магазином:
@@ -30,21 +32,34 @@ console.log('Задание №2 с корзиной интернет магаз
 /*Корзина с выбранным товаром\количество\цена 
 храним в виде объекта */
 var basketProducts = [
-   {name: 'apple', price: 100, quantity: 2},
+   {name: 'apple', price: 100, quantity: 1},
    {name: 'orange', price: 300, quantity: 1},
-   {name: 'mango', price: 100, quantity: 3}
+   {name: 'mango', price: 100, quantity: 1}
 ];
 
 // функция подсчета стоимости корзины  countBasketPrice()
-var countBasketPrice = function(param) {
-   var moneyProduct = 0;
-   for (let n in param) {
-   var product = param[n];
-   moneyProduct += (product['price'] * product['quantity']);
-   } return moneyProduct;
-   
-}
-console.log('Итого сумма корзины ' + countBasketPrice(basketProducts));
+//1  вариант решения 
+
+//var countBasketPrice = function(param) {
+//    var moneyProduct = 0;
+//    for (let n in param) {
+//    var product = param[n];
+//    moneyProduct += (product['price'] * product['quantity']);
+//    } return moneyProduct;
+// }
+
+//2 вариант решения 
+var countBasketPrice = function() {
+   let sum = 0;
+
+   for (let i = 0; i < basketProducts.length; i++) {
+      sum += basketProducts[i].price * basketProducts[i].quantity;
+   }
+   console.log(sum);
+};
+countBasketPrice();
+
+
 
 
 
